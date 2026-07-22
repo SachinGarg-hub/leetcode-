6e15a54249b3c28bd7c6b9df51bde84288d0d039
Sachin garg <gargsachin809@gmail.com>
2026-07-23 05:10:05 +0530

    Time: 3 ms (33.72%), Space: 9.2 MB (93.53%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -3,7 +3,7 @@ public:
     string removeOccurrences(string s, string part) {
         int start = s.find(part);
 
-        while (start != string::npos) {
+        while (s.length()>0 && start<s.length()) {
             s.erase(start, part.length());
             start = s.find(part);
         }
